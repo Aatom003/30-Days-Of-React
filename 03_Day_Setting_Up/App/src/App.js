523cdad61782ac94c7ff74c6655dfabad040b372
Anind Jha <Anindkmr@gmail.com>
2021-10-22 21:50:32 +0530

    Day 3 Solution

--- 03_Day_Setting_Up/App/src/App.js
+++ 03_Day_Setting_Up/App/src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello
+          {/* Edit <code>src/App.js</code> and save to reload. */}
         </p>
         <a
           className="App-link"
@@ -20,6 +21,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
--- 03_Day_Setting_Up/App/src/index.js
+++ 03_Day_Setting_Up/App/src/index.js
@@ -1,14 +1,82 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+// import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+// import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
+const header = (
+  <header>
+    <center>
+      <h1 className="h1">SUBSCRIBE</h1>
+      <p>Sign up with your email address to receive news and updates. </p>
+
+      <table>
+        <td>
+          <input
+            className="abc"
+            type="text"
+            name="name"
+            placeholder="First name"
+          />{" "}
+          <input
+            className="abc"
+            type="text"
+            name="name"
+            placeholder="Last name"
+          />{" "}
+          <input className="abc" type="text" name="name" placeholder="Email" />{" "}
+        </td>
+      </table>
+
+      <button
+        type="button"
+        onClick={console.log("hi")}
+        className="btn btn-danger c_button"
+      >
+        SUBSCRIBE
+      </button>
+    </center>
+  </header>
+);
+
+const main = (
+  <main>
+    {/* <table>
+      <td classNameName="abc">
+        <input type="text" name="name" placeholder="First name" />{" "}
+        <input type="text" name="name" placeholder="Last name" />{" "}
+        <input type="text" name="name" placeholder="Email" />{" "}
+      </td>
+    </table>
+    <center>
+      <button type="button" className="btn btn-danger">
+        Danger
+      </button>
+    </center> */}
+  </main>
+);
+
+// JSX element, footer
+const footer = (
+  <div>
+    {/* <center>
+      <button type="button" className="btn btn-danger">
+        Danger
+      </button>
+    </center> */}
+  </div>
+);
+
+// ReactDOM.render(jsxElement, rootElement);
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {header}
+    {main}
+    {footer}
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
